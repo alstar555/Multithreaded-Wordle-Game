cdacd325281c6151e89c45679379b00218507de4
Angelica <67800009+alstar555@users.noreply.github.com>
2024-07-26 22:16:22 -0600

    Add files via upload

---
+++ wordle_game/main.c
@@ -0,0 +1,34 @@
+/*main.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int total_guesses;
+int total_wins;
+int total_losses;
+char ** words;
+
+
+int wordle_server( int argc, char ** argv );
+
+int main( int argc, char ** argv )
+{
+  total_guesses = total_wins = total_losses = 0;
+  words = calloc( 1, sizeof( char * ) );
+  if ( words == NULL ) { perror( "calloc() failed" ); return EXIT_FAILURE; }
+
+  int rc = wordle_server( argc, argv );
+
+
+  for ( char ** ptr = words ; *ptr ; ptr++ )
+  {
+    printf( "WORD: %s\n", *ptr );
+  }
+
+  /* deallocate memory for the list of words played */
+  for ( char ** ptr = words ; *ptr ; ptr++ ) free( *ptr );
+  free( words );
+
+  return rc;
+}
